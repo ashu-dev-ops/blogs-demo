8430483c258dad669165f64fbd9aa1baec29dd5c
ashu-dev-ops <computerlogic25@gmail.com>
2023-10-31 17:00:08 +0530

    dummy blog added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,18 @@
+const { default: axios } = require("axios");
 const express = require("express");
 const router = express.Router();
-router.get("/", (req, res) => {
-  res.render("index");
+router.get("/", async (req, res) => {
+  const data = await axios.get(
+    "https://jsonplaceholder.typicode.com/posts?_page=1&_limit=5"
+  );
+  console.log(data.data);
+  res.render("index", { data: data.data });
+});
+router.get("/blog/:id", async (req, res) => {
+  const data = await axios.get(
+    `https://jsonplaceholder.typicode.com/posts/${req.params.id}`
+  );
+  console.log(data.data);
+  res.render("blog", { data: data.data });
 });
 module.exports = router;
